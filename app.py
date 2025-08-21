e2bdb8188d1c940669879ca6df79676eeb60b709
Bhavin Parmar <2203031080042@paruluniversity.ac.in>
2025-08-21 10:56:18 +0530

    version 0.11

--- app.py
+++ app.py
@@ -6,15 +6,13 @@ import numpy as np
 import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "static"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['image']
         if file:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], "uploaded.jpg")
+            filepath = os.path.join("/tmp", "uploaded.jpg")
             file.save(filepath)
 
             # Convert to SSTV
@@ -23,7 +21,7 @@ def index():
             samples = np.fromiter(sstv.gen_samples(), dtype=np.float32)
             samples = np.clip(samples, -1.0, 1.0)
             samples = (samples * 32767).astype(np.int16)
-            out_wav_path = os.path.join(app.config['UPLOAD_FOLDER'], "output_sstv.wav")
+            out_wav_path = os.path.join("/tmp", "output_sstv.wav")
             write(out_wav_path, 44100, samples)
 
             return render_template('index.html', generated=True)
@@ -32,7 +30,4 @@ def index():
 
 @app.route('/download')
 def download():
-    return send_file("static/output_sstv.wav", as_attachment=True)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return send_file("/tmp/output_sstv.wav", as_attachment=True)
